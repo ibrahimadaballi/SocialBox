6753a28ccb77877d065411e1597adb90be702a88
İbrahim Adaballi <114339796+ibrhim21@users.noreply.github.com>
2024-01-13 23:38:58 +0300

    Add files via upload

--- install-sb.sh
+++ install-sb.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+git pull >> /dev/null
+chmod 777 -R *
+chmod 777 -R .
 #Sysupdate
-apt-get update 
-#Python
-apt-get install python
+apt-get update -qq >> /dev/null
+#apt full-upgrade 
+#req
+apt install python2 python tor perl figlet runit openssl -y >> /dev/null
+clear
+sleep 2
+figlet Installing ...
 #gmail
-git clone https://github.com/Ha3MrX/Gemail-Hack.git
+figlet gmail bf
+git clone https://github.com/Ha3MrX/Gemail-Hack.git >> /dev/null
+sleep 2
+clear
+figlet Installing ...
+figlet facebook bf
 #facebook
 if [ -d facebook ]; then
         echo "Directory facebook exists.[✔]"
@@ -14,17 +26,34 @@ else
 	wget raw.githubusercontent.com/Sup3r-Us3r/scripts/master/fb-brute.pl
 	cd ..
 fi
-#Instagram
- git clone https://github.com/ibrhim21/SocialBox/tree/main/instainsane.git
+sleep 2
+clear
+figlet Installing ...
+figlet instagram bf1
+#Instagram 1
+#git clone https://github.com/umeshshinde19/instainsane.git
 cd instainsane
 chmod +x instainsane.sh
 chmod +x install.sh
- ./install.sh
+./install.sh
 cd ..
+sleep 2
+clear
+figlet Installing ...
+figlet twitter bf
 #Twitter
-git clone https://github.com/ibrhim21/SocialBox/tree/main/tweetshell.git
+#git clone https://github.com/imprithwishghosh/-tweetshell-.git
 cd tweetshell
 chmod +x tweetshell.sh
 chmod +x install.sh
 ./install.sh
 cd ..
+clear
+figlet Installing ...
+figlet instagram bf 2
+# instagram 2
+git clone https://github.com/samsesh/insta-bf.git
+cd insta-bf 
+bash ./andriod_setup.sh	>> /dev/null
+clear
+figlet done
