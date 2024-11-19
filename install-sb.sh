8476263ce93fe50ef010cba6a695d4bcca28a74e
İbrahim Adaballi <114339796+ibrahimadaballi@users.noreply.github.com>
2024-11-20 00:50:11 +0300

    Update install-sb.sh

--- install-sb.sh
+++ install-sb.sh
@@ -14,8 +14,8 @@ figlet Installing ...
 figlet gmail bf
 git clone https://github.com/ibrahimadaballi/SocialBox/Gemail-Hack.git >> /dev/null
 cd Gemail-Hack
-chmod +x instainsane.sh
-chmod +x install.sh
+chmod +x gemailhack.py
+python gemailhack.py
 ./install.sh
 cd ..
 sleep 2
